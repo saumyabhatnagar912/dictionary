6d0721ed574a674d475f745e88626e5fcd38b54e
saumyabhatnagar912 <saumyabhatnagar912@gmail.com>
2019-06-25 22:15:18 -0400

    updates to the dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,12 +1,29 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 query = input("Enter a word: ")
 query = query.lower()
 def find_meaning(w):
-    try:
+    if w in data:
         return data[w]
-    except:
-        return "Word does not exist"
-print(find_meaning(query))
+    elif len(get_close_matches(w,data.keys())) > 0:
+        print("Did you mean %s?" %get_close_matches(w,data.keys())[0])
+        user_input = input("Type Y for yes, N for no: ")
+        if user_input == "Y" or user_input =="y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif user_input == "N" or user_input =="n":
+            return "%s word does not exist" %w
+        else:
+            return "We do not understand your entry, try again!"
+    else:
+        return "%s word does not exist" %w
+
+output = find_meaning(query)
+
+if type(output) is list:
+    for o in output:
+        print(o)
+else:
+    print(output)
